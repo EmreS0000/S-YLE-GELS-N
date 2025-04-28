d893a60cee997ab656105611b866e1ce1ba60df3
zeynepmecni2003 <163599719+zeynepp2003@users.noreply.github.com>
2025-04-29 00:21:57 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,79 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
-import LoginForm from './Components/LoginForm/LoginForm';
+
+import LoginRegister from './Components/LoginRegister/LoginRegister';
+import MenuPage from './Components/LoginRegister/MenuPage';
+import CheckoutPage from './Components/LoginRegister/CheckoutPage';
+import CreditCardPage from './Components/LoginRegister/CreditCardPage';
+import PaymentSuccessPage from './Components/LoginRegister/PaymentSuccessPage'; 
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false); 
+  const [userInfo, setUserInfo] = useState({}); 
+
   return (
-    <div >
-     <LoginForm />
-    </div>
+    <Router>
+      <Routes>
+
+        
+        <Route 
+          path="/" 
+          element={
+            isLoggedIn 
+              ? <Navigate to="/menu" /> 
+              : <LoginRegister setIsLoggedIn={setIsLoggedIn} setUserInfo={setUserInfo} />
+          } 
+        />
+
+     
+        <Route 
+          path="/menu" 
+          element={
+            isLoggedIn 
+              ? <MenuPage userInfo={userInfo} /> 
+              : <Navigate to="/" />
+          } 
+        />
+
+        
+        <Route 
+          path="/payment" 
+          element={
+            isLoggedIn 
+              ? <CheckoutPage /> 
+              : <Navigate to="/" />
+          } 
+        />
+
+       
+        <Route 
+          path="/credit-card" 
+          element={
+            isLoggedIn 
+              ? <CreditCardPage /> 
+              : <Navigate to="/" />
+          } 
+        />
+
+        
+        <Route 
+          path="/success" 
+          element={
+            isLoggedIn 
+              ? <PaymentSuccessPage userInfo={userInfo} /> 
+              : <Navigate to="/" />
+          } 
+        />
+
+        
+        <Route 
+          path="*" 
+          element={<Navigate to="/" />} 
+        />
+
+      </Routes>
+    </Router>
   );
 }
 
